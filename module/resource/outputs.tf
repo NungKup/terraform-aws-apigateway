42a628b4e720b36ac451a735679d209b17654ade
thanakritnil <thanakritnil@gosoft.co.th>
2023-08-21 13:31:16 +0700

    update

--- module/resource/outputs.tf
+++ module/resource/outputs.tf
@@ -1,6 +1,6 @@
 output "api_resource_id" {
-  value = aws_api_gateway_resource.default[*].id
+  value = try(aws_api_gateway_resource.default[*].id, null)
 }
 output "api_resource_path" {
-  value = aws_api_gateway_resource.default[*].path
+  value = try(aws_api_gateway_resource.default[*].path, null)
 }
