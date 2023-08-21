d67301fc4fd9e260df8165b04288a542baaef4df
thanakritnil <thanakritnil@gosoft.co.th>
2023-08-21 11:00:44 +0700

    Update Tags VPC Link

--- module/rest_api/main.tf
+++ module/rest_api/main.tf
@@ -64,4 +64,6 @@ resource "aws_api_gateway_vpc_link" "default" {
   name        = var.vpc_link_name
   description = var.vpc_link_description
   target_arns = var.nlb_target_arn
+
+  tags = { Name = var.vpc_link_name }
 }
