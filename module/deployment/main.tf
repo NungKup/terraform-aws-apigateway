1789e4b1145c8dfe80ab28e6a745d3529f4a3f33
thanakritnil <thanakritnil@gosoft.co.th>
2023-08-21 17:31:24 +0700

    Update

--- module/deployment/main.tf
+++ module/deployment/main.tf
@@ -2,8 +2,8 @@ resource "aws_api_gateway_deployment" "default" {
   for_each = var.deploy
 
   rest_api_id = var.api_id
-  stage_name  = lookup(each.value, "stage_name", "")
-  description = lookup(each.value, "deployment_description", "")
+  stage_name  = try(each.value.stage_name, "")
+  description = try(each.value.deployment_description, "")
   # stage_description = lookup(each.value, "stage_description", "")
   # variables         = lookup(each.value, "stage_variables", "")
   #   depends_on        = [aws_api_gateway_method.default, aws_api_gateway_integration.default]
@@ -13,5 +13,5 @@ resource "aws_api_gateway_stage" "default" {
   for_each      = var.stage
   deployment_id = aws_api_gateway_deployment.default[lookup(each.value, "deployment_id", "")].id
   rest_api_id   = var.api_id
-  stage_name    = lookup(each.value, "stage_name", "")
+  stage_name    = try(each.value.stage_name, "")
 }
