4141414ea6e26bc1421daa71266f3de8cc057fa9
thanakritnil <thanakritnil@gosoft.co.th>
2023-08-21 13:43:44 +0700

    update

--- module/resource/outputs.tf
+++ module/resource/outputs.tf
@@ -1,5 +1,5 @@
 output "api_resource_id" {
-  value = try({ for k, v in aws_api_gateway_resource.default : k => v.id }, "")
+  value = concat(try({ for k, v in aws_api_gateway_resource.default : k => v.id }, ""), try({ for k, v in aws_api_gateway_resource.parent_id : k => v.id }, ""))
   #   value = try(aws_api_gateway_resource.default[*].id, null)
 }
 # output "api_resource_path" {
