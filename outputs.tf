dc0f17722d78976c71b1ea37faa3fdf527305e7d
Thanakrit Niljantuek <45716791+thanakrit1997@users.noreply.github.com>
2023-10-17 14:33:00 +0700

    Update outputs.tf

--- outputs.tf
+++ outputs.tf
@@ -7,9 +7,9 @@ output "api_root_resource_id" {
 output "api_arn" {
   value = module.reat_api.api_arn
 }
-# output "api_vpc_link" {
-#   value = module.reat_api.api_vpc_link
-# }
+output "api_vpc_link" {
+  value = module.vpclink.api_vpc_link
+}
 
 output "api_resource_id" {
   value = module.resource.api_resource_id
